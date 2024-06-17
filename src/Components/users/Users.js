c8a763369eebb4f13178ab308fd0c2e646e4c172
tchanque <audevie.thomas@gmail.com>
2024-06-17 11:18:16 +0200

    login is working

---
+++ src/Components/users/Users.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Users = () => {
+    const [users, setUsers] = useState();
+
+    return (
+        <article>
+            <h2>Users List</h2>
+            
+        </article>
+    )
+}
+
+export default Users;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,46 @@
+import { Input } from "@nextui-org/react";
+import { axiosPrivate } from "../../api/axios";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axiosPrivate
+      .post("/users/sign_in", { 'user': {'email': email, 'password': password} })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+        } else {
+          console.error(error);
+        }
+      });
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          label="Mot de passe"
+          placeholder=""
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="Submit">Connexion</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
