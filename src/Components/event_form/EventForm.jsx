19a0e5079c13dbb80d118e75ef4b5b61c0937fc3
Marcaraph <ra.marcarini@gmail.com>
2024-06-21 12:39:12 +0200

    PopUp EventForm
    Co-authored-by: evarellapucky <evarellapucky@users.noreply.github.com>

--- src/Components/event_form/EventForm.jsx
+++ src/Components/event_form/EventForm.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import { format } from "date-fns";
 import { axiosPrivate } from "../../api/axios";
 import { useAtom } from "jotai";
-import { bearerTokenAtom } from "../../atom/atoms";
+import { bearerTokenAtom, popUpAtom } from "../../atom/atoms";
 import EventInstrumentForm from "./event_instrument_form/EventInstrumentForm";
-
+import "./EventForm.css";
 
 const EventForm = () => {
   const [category, setCategory] = useState("");
@@ -21,6 +21,8 @@ const EventForm = () => {
   const [token, setToken] = useAtom(bearerTokenAtom);
 
   const [eventInstruments, setEventInstruments] = useState([]);
+
+  const [popUp, setPopUp] = useAtom(popUpAtom);
 
   const addEventInstrumentForm = (e) => {
     e.preventDefault();
@@ -47,6 +49,10 @@ const EventForm = () => {
   const printInstruments = (e) => {
     e.preventDefault();
     eventInstruments.map((instrument) => console.log(instrument));
+  };
+
+  const handleClosePopUp = () => {
+    setPopUp(false);
   };
 
   useEffect(() => {
@@ -111,9 +117,14 @@ const EventForm = () => {
                 }
               )
               .then((response) => {
+                handleClosePopUp();
+                window.location.reload();
                 console.log(response);
               });
           });
+        } else {
+          handleClosePopUp();
+          window.location.reload();
         }
       })
       .catch((error) => {
@@ -121,12 +132,29 @@ const EventForm = () => {
       });
   };
 
+  if (popUp)
   return (
     <section className="dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center my-32 px-6 py-8 mx-auto lg:py-0">
-        <div className="bg-white rounded-lg shadow w-200 dark:border md:mt-0 sm:max-w-2xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-center font-Ubuntu text-primary-dark md:text-2xl dark:text-white">
+      <div className={`modal ${popUp ? 'is-active' : ''}`}>
+        <div className=" modal-overlay">
+          <div className="modal-content">
+            <button onClick={handleClosePopUp} className="modal-close">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="25"
+                height="25"
+                viewBox="0 0 50 50"
+                fill="#F31248"
+              >
+                <path d="M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 37.690466 48 48 37.690466 48 25 C 48 12.309534 37.690466 2 25 2 z M 25 4 C 36.609534 4 46 13.390466 46 25 C 46 36.609534 36.609534 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 z M 32.990234 15.986328 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.990234 15.986328 z"
+                stroke="#F31248"
+                strokeWidth={1.5}
+                ></path>
+              </svg>
+            </button>
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-center font-Ubuntu mb-8 text-primary-dark md:text-2xl dark:text-white">
               CRÉATION D'ÉVÈNEMENT
             </h1>
 
--- src/atom/atoms.jsx
+++ src/atom/atoms.jsx
@@ -2,3 +2,4 @@ import { atomWithStorage } from "jotai/utils";
 
 export const bearerTokenAtom = atomWithStorage('token', "");
 export const currentUserAtom = atomWithStorage('currentUser', null);
+export const popUpAtom = atomWithStorage('popUp', null)
