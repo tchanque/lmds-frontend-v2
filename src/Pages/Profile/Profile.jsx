cdb48cf7e2e7ac5e2de7473cc56a391415e55543
tchanque <audevie.thomas@gmail.com>
2024-06-21 17:28:25 +0200

    temporary changes

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -5,15 +5,17 @@ import { useAtom } from "jotai";
 import { bearerTokenAtom, currentUserAtom } from "../../atom/atoms";
 import avatar from "../../public/images/profile_picture.jpeg";
 
-// "../public/images/profile_picture.jpeg"
 import "./profile.css";
 
 function Profile() {
   const { id } = useParams();
+
   const [currentUser] = useAtom(currentUserAtom);
   const [token] = useAtom(bearerTokenAtom);
+
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [modifyMenu, setModifyMenu] = useState(false);
 
   useEffect(() => {
     if (token) {
@@ -26,6 +28,7 @@ function Profile() {
           withCredentials: true,
         })
         .then((response) => {
+          console.log(response.data)
           setUser(response.data.user);
           setLoading(false);
         })
@@ -46,8 +49,68 @@ function Profile() {
   if (!token) return <div>Vous n'êtes pas autorisé</div>;
   if (!user) return <div>Pas de profil correspondant</div>;
 
+  if (!modifyMenu)
+    return (
+      <section className="h-full">
+        <div className="title">
+          {user.id === currentUser.id ? (
+            <h1>MON COMPTE</h1>
+          ) : (
+            <h1>PROFIL MUSICIEN</h1>
+          )}
+        </div>
+        <div className="userProfileDetails bg-white mx-13 mt-24 p-10 rounded-lg">
+          <div className="mainInformationSection flex flex-col justify-center items-center">
+            <div className="w-64 h-64 rounded-full overflow-hidden justify-center">
+              <img
+                className="w-full h-full object-cover"
+                src={avatar}
+                alt="Logo"
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <p>
+                {user.first_name} {user.last_name}
+              </p>
+              <p>{user.email}</p>
+            </div>
+          </div>
+          <div className="secondaryInformationSection ml-10 relative">
+            <div className="flex">
+              {
+                user.skills.map((skill) => (
+                  <h1>skill.instrument</h1>
+                ))
+              }
+              <div className="mr-13" id="instrument1">
+                <p>Piano</p>
+                <p>Niveau 3</p>
+              </div>
+              <div className="mr-13" id="instrument2">
+                <p>Guitare</p>
+                <p>Niveau 4</p>
+              </div>
+            </div>
+            <div className="description">
+              <h4>Description</h4>
+              <p>{user.description}</p>
+            </div>
+            <div className="absolute bottom-0 right-10">
+              <button
+                onClick={() => setModifyMenu(true)}
+                className="w-24 mt-10 text-white bg-info-main hover:bg-info-light font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Modifier
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+
   return (
-    <section>
+    <section className="h-full">
+      <form action=""></form>
       <div className="title">
         {user.id === currentUser.id ? (
           <h1>MON COMPTE</h1>
@@ -55,40 +118,59 @@ function Profile() {
           <h1>PROFIL MUSICIEN</h1>
         )}
       </div>
-      <div className="userProfileDetails">
-        <div className="mainInformationSection">
-          <div>
-            <img src={avatar} alt="Logo" />
+      <div className="userProfileDetails bg-white mx-13 mt-24 p-10 rounded-lg">
+        <div className="mainInformationSection flex flex-col justify-center items-center">
+          <div className="w-64 h-64 rounded-full overflow-hidden justify-center">
+            <img
+              className="w-full h-full object-cover"
+              src={avatar}
+              alt="Logo"
+            />
           </div>
-          <div>
+          <div className="flex flex-col items-center">
             <p>
               {user.first_name} {user.last_name}
             </p>
             <p>{user.email}</p>
           </div>
         </div>
-        <div className="secondaryInformationSection">
-          <div className="instrumentSection">
-            <div>
+        <div className="secondaryInformationSection ml-10 relative">
+          <div className="flex">
+            <div className="mr-13" id="instrument1">
               <p>Piano</p>
               <p>Niveau 3</p>
             </div>
-            <div>
+            <div className="mr-13" id="instrument2">
               <p>Guitare</p>
               <p>Niveau 4</p>
             </div>
           </div>
           <div className="description">
-            <p>{user.description}</p>
+            <h4>Description</h4>
+            <input type="text" placeholder={user.description} />
           </div>
-          <div className="passwordSection">
-            <h4>Changer mon mot de passe</h4>
-            <input type="text" placeholder="Ancien mot de passe" />
-            <input type="text" placeholder="Nouveau mot de passe" />
-            <input type="text" placeholder="Confirmer nouveau mot de passe" />
+          <div className="absolute bottom-0 right-10 flex space-x-5">
+            <button
+              onClick={() => setModifyMenu(false)}
+              className="w-24 text-white bg-warning-main hover:bg-warning-light font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Retour
+            </button>
+            <button
+              onClick={() => setModifyMenu(false)}
+              className="w-24 text-white bg-success-main hover:bg-success-light font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Valider
+            </button>
           </div>
         </div>
       </div>
+      {/* <div className="flex flex-col">
+            <h4>Changer mon mot de passe</h4>
+            <input className="w-1/2 my-2 px-4 py-2 border rounded-md" type="text" placeholder="Ancien mot de passe" />
+            <input className="w-1/2 my-2 px-4 py-2 border rounded-md" placeholder="Nouveau mot de passe" />
+            <input className="w-1/2 my-2 px-4 py-2 border rounded-md" placeholder="Confirmer nouveau mot de passe" />
+          </div> */}
     </section>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import {NextUIProvider} from "@nextui-org/react";
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-    {/* Don't forget to add dark class with btn in main tag */}
-    <main className="light text-foreground">
+      {/* Don't forget to add dark class with btn in main tag */}
+      <main className="light text-foreground h-screen">
         <App />
       </main>
     </NextUIProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
