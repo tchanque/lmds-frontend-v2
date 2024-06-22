552535b370ab3ae99dc60b79b0f5336aa3a8f897
evarellapucky <scherer.alexia@gmail.com>
2024-06-22 16:58:44 +0200

    Affichage carousel fonctionnel (pas encore avec le to_display)

--- src/Components/show_publications/AllPublications.jsx
+++ src/Components/show_publications/AllPublications.jsx
@@ -3,6 +3,11 @@ import { useState, useEffect } from "react";
 import { bearerTokenAtom, currentUserAtom } from "../../atom/atoms";
 import { useAtom } from "jotai";
 import {Button} from "@nextui-org/react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./all_publications.css"
+
 
 
 function AllPublications() {
@@ -39,42 +44,52 @@ function AllPublications() {
 
 console.log("this is ALL the publications :", allPublications);
 
+const littleDescription = (text, maxLength) => {
+  if (text.length <= maxLength) 
+    return text;
+  return text.substring(0, maxLength) + "...";
+};
 
+const formatDate = (date) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+  return new Date(date).toLocaleDateString("fr-FR", options);
+};
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
 
   return (
-    <>
-    <h1>ALL PUBLICATIONS HERE</h1>
-    <div className="flex">
-    
-      <div className="flex flex-col">
-        {allPublications.map((publication) => (
-          <div
-            key={publication.id}
-            className="flex items-center self-center justify-around w-4/6 gap-5 p-5 m-5 bg-white shadow h-72"
-          >
-            <div className="w-80">
-              <img
-                className=""
-                src="https://cdn.pixabay.com/photo/2024/05/18/08/16/cat-8769861_1280.jpg"
-              />
-            </div>
-
-            <div className="flex flex-col items-center gap-0">
-              <div className="flex flex-col items-center gap-0">
-                <p className="">{publication.title}</p>
-              </div>
-              <h3 className="underline">Description</h3>
-              <p>{publication.description}</p>
-              <div className="flex gap-10">
-                <Button className="text-white bg-success-main">Voir plus</Button>
+    <div className="max-w-screen-lg mx-auto">
+    <Slider {...settings}>
+      {allPublications.map((publication) => (
+        <div key={publication.id} className="relative">
+          <img
+            className="w-full rounded-lg"
+            src="https://cdn.pixabay.com/photo/2024/05/18/08/16/cat-8769861_1280.jpg"
+            alt={publication.title}
+          />
+           <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-white text-black w-full sm:w-3/4 p-4 shadow-2xl mb-4 text-center">
+           <p className="text-gray-500 text-sm mb-2">{formatDate(publication.created_at)}</p>
+              <h2 className="text-primary-dark text-2xl font-bold mb-2">{publication.title}</h2>
+              <p className="text-primary-dark text-sm mb-4">{littleDescription(publication.description, 300)}</p>
+              <div className="flex justify-end">
+                <button className="text-white bg-primary-main py-2 px-4 rounded">Voir plus</button>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  </>
+        </div>
+      ))}
+    </Slider>
+  </div>
   )
 }
 
