6c5c25f5f470a17c5676b9226e441942e9cd7b0c
tchanque <audevie.thomas@gmail.com>
2024-06-26 15:11:04 +0200

    design ok for large screens, working on responsive now

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -85,24 +85,20 @@ function Profile() {
 
   const handleDelete = async () => {
     try {
-      axiosPrivate.delete(
-        `/users/${id}`,
-        {headers: 
-          {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-          },
-          withCredentials: true
-        }
-      )
+      axiosPrivate.delete(`/users/${id}`, {
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
       setToken("");
       setCurrentUser("");
       navigate("/");
+    } catch (error) {
+      console.error("Error deleting the profile: ", error);
     }
-    catch (error) {
-      console.error("Error deleting the profile: ", error)
-    }
-  }
+  };
 
   const handleChangePassword = async () => {
     try {
@@ -141,148 +137,155 @@ function Profile() {
 
   return (
     <>
-    <section className="h-full">
-      <div className="title">
-        {user.id === currentUser.id ? (
-          <h1>MON COMPTE</h1>
-        ) : (
-          <h1>PROFIL MUSICIEN</h1>
-        )}
-      </div>
-      <div className="userProfileDetails bg-white mx-13 mt-24 p-10 rounded-lg">
-        <div className="mainInformationSection flex flex-col justify-center items-center">
-          <div className="w-64 h-64 rounded-full overflow-hidden justify-center">
-            <img
-              className="w-full h-full object-cover"
-              src={avatar}
-              alt="Logo"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            {modifyMenu && user.id === currentUser.id ? (
-              <>
-                <input
-                  type="text"
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  placeholder="First Name"
-                  className="w-1/2 my-2 px-4 py-2 border rounded-md"
-                />
-                <input
-                  type="text"
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  placeholder="Last Name"
-                  className="w-1/2 my-2 px-4 py-2 border rounded-md"
-                />
-                <p>{user.email}</p>
-              </>
-            ) : (
-              <>
-                <p>
-                  {firstName} {lastName}
-                </p>
-                <p>{user.email}</p>
-              </>
-            )}
-          </div>
+      <section className="h-full">
+        <div className="title">
+          {user.id === currentUser.id ? (
+            <h1>MON COMPTE</h1>
+          ) : (
+            <h1>PROFIL MUSICIEN</h1>
+          )}
         </div>
-        <div className="secondaryInformationSection ml-10 relative">
-          <div className="flex">
-            {user.skills.map((skill, index) => (
-              <div className="mr-13" key={`instruments${index}`}>
-                <p>{skill.instrument.name}</p>
-                <p>{`Niveau ${skill.level}`}</p>
-              </div>
-            ))}
-          </div>
-          <div className="description">
-            <h4>Description</h4>
-            {modifyMenu && user.id === currentUser.id ? (
-              <input
-                type="text"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="Description"
-                className="w-full my-2 px-4 py-2 border rounded-md"
+        <div className="userProfileDetails bg-white mx-13 mt-24 p-10 rounded-lg flex flex-col md:flex-col">
+          <div className="mainInformationSection flex flex-col justify-center items-center">
+            <div className="w-64 h-64 rounded-full overflow-hidden justify-center my-2">
+              <img
+                className="w-full h-full object-cover"
+                src={avatar}
+                alt="Logo"
               />
-            ) : (
-              <p>{description}</p>
-            )}
+            </div>
+
+            <div className="flex">
+              {modifyMenu && user.id === currentUser.id ? (
+                <>
+                  <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="First Name"
+                    className="w-1/2 py-2 border rounded-md text-grey-main font-Ubuntu text-center"
+                  />
+                  <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Last Name"
+                    className="w-1/2 py-2 border rounded-md text-grey-main font-Ubuntu text-center"
+                  />
+                </>
+              ) : (
+                <>
+                  <p>
+                    {firstName} {lastName}
+                  </p>
+                </>
+              )}
+            </div>
+            <p>{user.email}</p>
           </div>
-          <div className="absolute bottom-0 right-10">
-            {modifyMenu && user.id === currentUser.id ? (
-              <>
-                <button
-                  onClick={() => setModifyMenu(false)}
-                  className="w-24 mt-10 text-white bg-danger-main hover:bg-danger-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Annuler
-                </button>
-                <button
-                  onClick={handleSave}
-                  className="w-24 mt-10 text-white bg-success-main hover:bg-success-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ml-5"
-                >
-                  Sauvegarder
-                </button>
-              </>
-            ) : (
-              user.id === currentUser.id && (
+          <div className="secondaryInformationSection ml-10 relative">
+            <div className="flex">
+              {user.skills.map((skill, index) => (
+                <div className="mr-13" key={`instruments${index}`}>
+                  <p>{skill.instrument.name}</p>
+                  <p>{`Niveau ${skill.level}`}</p>
+                </div>
+              ))}
+            </div>
+            <div className="description">
+              <h4>Description</h4>
+              {modifyMenu ? (
+                <textarea
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  placeholder="Description"
+                  className="w-full my-4 px-2 py-2 border rounded-md break-text text-grey-main font-Ubuntu"
+                />
+              ) : (
+                <p className="w-full my-4 px-2 py-2">{description}</p>
+              )}
+            </div>
+            <div className="absolute bottom-0 right-10">
+              {modifyMenu ? (
                 <>
                   <button
-                    onClick={() => setModifyMenu(true)}
-                    className="w-24 mt-10 text-white bg-info-main hover:bg-info-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    onClick={() => setModifyMenu(false)}
+                    className="w-24 mt-10 text-white bg-warning-main hover:bg-warning-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   >
-                    Modifier
+                    Annuler
                   </button>
                   <button
-                    onClick={handleDelete}
-                    className="w-24 mt-10 text-white bg-danger-main hover:bg-danger-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    onClick={handleSave}
+                    className="w-24 mt-10 text-white bg-success-main hover:bg-success-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ml-5"
                   >
-                    Supprimer
+                    Sauvegarder
                   </button>
                 </>
-              )
-            )}
+              ) : (
+                (user.id === currentUser.id ||
+                  currentUser.role === "Admin") && (
+                  <>
+                    <button
+                      onClick={() => setModifyMenu(true)}
+                      className="w-24 mt-10 text-white bg-info-main hover:bg-info-light font-medium rounded-lg text-sm py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      Modifier
+                    </button>
+                    <button
+                      onClick={handleDelete}
+                      className="w-24 mt-10 text-white bg-danger-main hover:bg-danger-light font-medium rounded-lg text-sm py-2.5 ml-5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      Supprimer
+                    </button>
+                  </>
+                )
+              )}
+            </div>
           </div>
         </div>
-        {user.id === currentUser.id && (
-          <div className="changePasswordSection mt-10">
-            <h4>Changer mon mot de passe</h4>
-            <input
-              className="w-1/2 my-2 px-4 py-2 border rounded-md"
-              type="password"
-              value={oldPassword}
-              onChange={(e) => setOldPassword(e.target.value)}
-              placeholder="Ancien mot de passe"
-            />
-            <input
-              className="w-1/2 my-2 px-4 py-2 border rounded-md"
-              type="password"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-              placeholder="Nouveau mot de passe"
-            />
-            <input
-              className="w-1/2 my-2 px-4 py-2 border rounded-md"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              placeholder="Confirmer nouveau mot de passe"
-            />
-            <button
-              onClick={handleChangePassword}
-              className="w-24 mt-10 text-white bg-info-main hover:bg-info-light font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Changer
-            </button>
+
+        {user.id === currentUser.id && ( // only the user can change its password
+          <div
+            id="passwordSection"
+            className="bg-white mx-13 mt-24 p-10 rounded-lg"
+          >
+            <div className="flex flex-col">
+              <h4>Changer mon mot de passe</h4>
+              <input
+                className="w-1/2 my-2 px-4 py-2 border rounded-md"
+                type="password"
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+                placeholder="Ancien mot de passe"
+              />
+              <input
+                className="w-1/2 my-2 px-4 py-2 border rounded-md"
+                type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                placeholder="Nouveau mot de passe"
+              />
+              <input
+                className="w-1/2 my-2 px-4 py-2 border rounded-md"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirmer nouveau mot de passe"
+              />
+              <button
+                onClick={handleChangePassword}
+                className="w-24 text-white bg-info-main hover:bg-info-light font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Changer
+              </button>
+            </div>
           </div>
         )}
-      </div>  
-    </section>
-    <div id="agenda">
-    {user.id === currentUser.id && <UserAgenda userId={id} />}
-    </div>
+        <div id="agenda">
+          {user.id === currentUser.id && <UserAgenda userId={id} />}
+        </div>
+      </section>
     </>
   );
 }
