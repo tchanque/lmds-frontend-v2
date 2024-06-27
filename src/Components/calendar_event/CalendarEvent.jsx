d032e8a59410b6b4c57dd7c8f4bc97e5ad2ad5e2
Damien-Generet <damiengeneret@outlook.com>
2024-06-27 09:28:56 +0200

    link css

--- src/Components/calendar_event/CalendarEvent.jsx
+++ src/Components/calendar_event/CalendarEvent.jsx
@@ -1,7 +1,7 @@
 // CalendarEvent.js
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import "./calendar_event.css"
 
 const CalendarEvent = ({ allEvents, onDateChange }) => {
   const [selectedDate, setSelectedDate] = useState(null);
